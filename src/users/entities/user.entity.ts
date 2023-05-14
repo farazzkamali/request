a9b2b48b71037984b171b98811da4567c0eba9f9
farazzkamali <farazzkamali80@gmail.com>
2023-05-14 11:28:26 -0400

    feat: add email & password hashing

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,7 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-
+import { InternalServerErrorException } from "@nestjs/common";
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import * as bcrypt from 'bcrypt';
+import { IsEmail, IsString } from "class-validator";
 
 @Entity()
 export class User {
@@ -7,8 +9,36 @@ export class User {
     id: number;
 
     @Column()
+    @IsEmail()
+    email: string;
+
+    @Column()
+    @IsString()
     username: string;
 
     @Column()
+    @IsString()
     password: string;
-}
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    async hashPassword(): Promise<void> {
+      if (this.password) {
+        try {
+          this.password = await bcrypt.hash(this.password, 10);
+        } catch (error) {
+          console.log(error);
+          throw new InternalServerErrorException();
+        }
+      }
+    }
+    async checkPassword(aPassword: string): Promise<boolean> {
+      try {
+        const ok = await bcrypt.compare(aPassword, this.password);
+        return ok;
+      } catch (error) {
+        console.log(error);
+        throw new InternalServerErrorException();
+      }
+    }
+  }
