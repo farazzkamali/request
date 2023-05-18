e38343cd3314636d1a2e2b6b8a6c763f5ae98136
farazzkamali <farazzkamali80@gmail.com>
2023-05-18 11:05:08 -0400

    feat: create

---
+++ src/gateway/gateway.ts
@@ -0,0 +1,12 @@
+import { MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+
+
+@WebSocketGateway()
+export class MyGateway {
+
+    @SubscribeMessage('newMessage')
+    onNewMessage(@MessageBody() body:any){
+        console.log(body)
+    }
+    
+}
